0f35cd551aa47255f66dc4de0798c6811423c7b8
gasick <gasick@mail.ru>
2019-06-18 20:35:44 +0300

    lesson 15

--- app/src/main/java/com/example/smack/Controllers/LoginActivity.kt
+++ app/src/main/java/com/example/smack/Controllers/LoginActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.smack.R
+import com.example.smack.Sevices.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,7 +16,16 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View){
+        val email = loginEmailTxt.text.toString()
+        val password = loginPasswordTxt.text.toString()
 
+        AuthService.loginUser(this, email, password){ loginSuccess ->
+            if (loginSuccess){
+                AuthService.findUserByEmail(this) { findSuccess ->
+                    if (findSuccess){finish()}
+                }
+            }
+        }
     }
 
     fun loginCreateUserBtnClicked(view: View){
