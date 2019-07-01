a1530958a4f24ef118a7cb8d94b81f3affa797e7
Pasha Gasanov (Akvelon INC) <v-pagasa@microsoft.com>
2019-06-30 19:12:38 -0700

    Send and receive messages

--- app/src/main/java/com/example/smack/Controllers/MainActivity.kt
+++ app/src/main/java/com/example/smack/Controllers/MainActivity.kt
@@ -17,6 +17,7 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import com.example.smack.Model.Channel
+import com.example.smack.Model.Message
 import com.example.smack.R
 import com.example.smack.Sevices.AuthService
 import com.example.smack.Sevices.MessageService
@@ -52,6 +53,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         socket.connect()
         socket.on("channelCreated",onNewChannel )
+        socket.on("messageCreated", onNewMessage  )
 
 
         channel_list.setOnItemClickListener{ _, _, i, _->
@@ -195,11 +197,32 @@ class MainActivity : AppCompatActivity() {
             println(newChannel.description)
             println(newChannel.id)
         }
+    }
 
+    private val onNewMessage = Emitter.Listener { args ->
+        runOnUiThread{
+            val msgBody = args[0] as String
+            val channelId = args[2] as String
+            val  userName = args[3] as String
+            val userAvatar = args[4] as String
+            val userAvatarColor = args[5] as String
+            val id = args[6] as String
+            val timeStamp = args[7] as String
+
+            val newMessage =  Message(msgBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
+            MessageService.messages.add(newMessage)
+            println(newMessage.message)
+        }
     }
 
     fun sendMessageBtnClicked(view: View){
-
+        if (App.prefs.isLoggedIn && messageTextField.text.isNotEmpty() && selectedChannel != null) {
+            val userId = UserDataService.id
+            val channelid = selectedChannel!!.id
+            socket.emit("newMessage", messageTextField.text.toString(), userId,
+                channelid, UserDataService.name, UserDataService.avatarName, UserDataService.avatarColor)
+            messageTextField.text.clear()
+        }
     }
 
 }
